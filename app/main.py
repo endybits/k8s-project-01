f560ba670d997f9ee96472bf8f10ae333ffbdde5
Endy B <endyb.dev@gmail.com>
2023-09-14 16:25:13 +1000

    Dockerfile

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"hello": "k8s"}
+    return {
+        "hello": "k8s",
+        "from hostname": f"{os.environ.get('HOSTNAME', 'DEFAULT_ENV')}"
+    }
